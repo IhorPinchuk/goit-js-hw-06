62a0ac442defcb4f929f301e78e9f6c177e9dd73
IhorPinchuk <pinchukkk@ukr.net>
2023-02-19 21:36:13 +0200

    task-10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,35 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  inputNumber: document.querySelector('#controls > input'),
+  createBtn: document.querySelector('[data-create]'),
+  destroyBtn: document.querySelector('[data-destroy]'),
+  boxes: document.querySelector('#boxes'),
+};
+
+refs.createBtn.addEventListener('click', divToAddBoxes);
+refs.destroyBtn.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const divToAddArray = [];
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement('div');
+    divEl.style.width = `${30 + 10 * i}px`;
+    divEl.style.height = `${30 + 10 * i}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    divToAddArray.push(divEl);
+  }
+  return divToAddArray;
+}
+
+function divToAddBoxes() {
+  let divToAdd = createBoxes(refs.inputNumber.value);
+  return refs.boxes.append(...divToAdd);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+  refs.inputNumber.value = '';
+}
